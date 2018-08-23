a8644a85fc3f25d1965cfaf4dd745d525fa5c51a
luosilent <luosilent@qq.com>
2018-08-23 10:27:39 +0800

    node upload img

--- nweb/requestHandlers.js
+++ nweb/requestHandlers.js
@@ -1,30 +1,66 @@
-var querystring = require("querystring");
-function start(response,postData) {
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
+
+function start(response) {
     console.log("Request handler 'start' was called.");
     var body = '<html>' +
         '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
+        '<meta http-equiv="Content-Type" ' +
+        'content="text/html; charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="5" cols="30"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
+        '<form action="/upload" enctype="multipart/form-data" ' +
+        'method="post">' +
+        '<input type="file" name="upload">' +
+        '<input type="submit" value="Upload file" />' +
         '</form>' +
         '</body>' +
         '</html>';
+
     response.writeHead(200, { "Content-Type": "text/html;charset=utf-8"});
-        response.write(body);
-        response.end();
+    response.write(body);
+    response.end();
     
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, { "Content-Type":"text/plain;charset=utf-8"});
-    response.write("You've sent: " + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function (error, fields, files) {
+        console.log("parsing done");
+        
+        // fs.renameSync(files.upload.path, "./nweb/temp/test.png");
+        var readStream = fs.createReadStream(files.upload.path);
+        var writeStream = fs.createWriteStream("./nweb/temp/test.png");
+        readStream.pipe(writeStream);
+        readStream.on('end', function () {
+        fs.unlinkSync(files.upload.path);
+        });
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    })
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("./nweb/temp/test.png","binary",function(error,file){
+        if(error){
+            response.writeHead(500,{"Content-Type":"text/plain"});
+            response.write(error + "\n");
+            response.end();
+        }else{
+            response.writeHead(200,{"Content-Type":"image/png"});
+            response.write(file,"binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;

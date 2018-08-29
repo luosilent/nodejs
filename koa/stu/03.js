3e8ae8ecb4876d79e37b692509ab3ea9d31fd16a
luosilent <luosilent@qq.com>
2018-08-29 17:02:50 +0800

    koa

--- koa/stu/03.js
+++ koa/stu/03.js
@@ -1,15 +1,35 @@
-const fs = require('fs');
-const Koa = require('koa');
-const app = new Koa();
+// 按照官方示例
+const Koa = require('koa')
+const app = new Koa()
 
-app.use(async(ctx) => {
-    if (ctx.path != '/') {
-        ctx.type = 'html';
-        ctx.body = '<a href = "/"> Index Page</a>';
-    }else{
-        ctx.type = 'html'
-        ctx.body = fs.createReadStream('./koa/tem/template.html')
-    }
+// 记录执行的时间
+app.use(async (ctx, next) => {
+    let stime = new Date().getTime()
+    await next()
+    let etime = new Date().getTime()
+    ctx.response.type = 'text/html'
+    ctx.response.body = '<h1>Hello World</h1>'
+    console.log(`请求地址: ${ctx.path}，响应时间：${etime - stime}ms`)
 });
 
-app.listen(3000);
+app.use(async (ctx, next) => {
+    console.log('中间件1 doSoming')
+    await next();
+    console.log('中间件1 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件2 doSoming')
+    await next();
+    console.log('中间件2 end')
+})
+
+app.use(async (ctx, next) => {
+    console.log('中间件3 doSoming')
+    await next();
+    console.log('中间件3 end')
+})
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000')
+})
